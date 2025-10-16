25d764a3c0d3e3a33436d0743a8e2fdebe30d44f
LucasAPessoa <lucasapessoa@outlook.com>
2025-10-16 12:08:49 -0300

    feat: add @fastify/cors dependency and configure CORS in server setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { appRoutes } from "./routes";
+import cors from "@fastify/cors";
 
 console.log("--- [PASSO 1] Iniciando o arquivo server.ts ---");
 
@@ -9,21 +10,24 @@ const app = Fastify({
 
 console.log("--- [PASSO 2] Instância do Fastify criada ---");
 
-app.register(appRoutes, { prefix: "/api" });
-
-console.log("--- [PASSO 3] Rotas registradas ---");
-
 const start = async () => {
     try {
+        await app.register(cors, {
+            origin: "http://localhost:5173",
+        });
+
+        app.register(appRoutes, { prefix: "/api" });
+        console.log("--- [PASSO 3] Rotas registradas ---");
+
         console.log("--- [PASSO 4] Dentro da função start ---");
 
-        // Verificando a variável de ambiente crucial
         if (!process.env.DATABASE_URL) {
             console.error(
                 "ERRO FATAL: A variável de ambiente DATABASE_URL não foi definida!"
             );
             throw new Error("DATABASE_URL não definida");
         }
+
         console.log(
             "--- [PASSO 5] DATABASE_URL encontrada. Tentando iniciar o servidor... ---"
         );
@@ -31,10 +35,8 @@ const start = async () => {
         const port = Number(process.env.PORT) || 3333;
         await app.listen({ port, host: "0.0.0.0" });
 
-        // Se você chegar aqui, a aplicação iniciou com sucesso!
         app.log.info(`🚀 Servidor HTTP rodando na porta ${port}`);
     } catch (err) {
-        // Este log vai capturar o erro exato que está quebrando a aplicação
         console.error("--- ERRO FATAL AO INICIAR O SERVIDOR ---");
         console.error(err);
         process.exit(1);
