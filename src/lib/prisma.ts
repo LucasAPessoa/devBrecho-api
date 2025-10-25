83b8a10c25cba6e528a965a0109c1b492ee9ece1
LucasAPessoa <lucasapessoa@outlook.com>
2025-10-25 19:16:03 -0300

    fix: update database URL environment variable from DB_POOL_URL to DATABASE_URL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient({
     datasources: {
         db: {
-            url: process.env.DB_POOL_URL,
+            url: process.env.DATABASE_URL,
         },
     },
 });
