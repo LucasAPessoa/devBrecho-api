bec5141f29caa6c5b822f3b0b43a9e6431ddf00e
LucasAPessoa <lucasapessoa@outlook.com>
2025-10-25 19:17:21 -0300

    refactor: remove Setor and PecaCadastrada routes for code cleanup

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { SetorController } from "./controllers/SetorController";
 import { FornecedoraController } from "./controllers/FornecedoraController";
 import { BolsaController } from "./controllers/BolsaController";
-import { PecaCadastradaController } from "./controllers/PecaCadastradaController";
 
 export async function appRoutes(app: FastifyInstance) {
-    // Rotas para Setor
-    app.get("/setor", SetorController.getSetores);
-    app.get("/setor/:id", SetorController.getSetor);
-    app.post("/setor", SetorController.createSetor);
-    app.put("/setor/:id", SetorController.updateSetor);
-    app.delete("/setor/:id", SetorController.deleteSetor);
-
     // Rotas para Fornecedora
     app.get("/fornecedora", FornecedoraController.getFornecedoras);
     app.get("/fornecedora/:id", FornecedoraController.getFornecedora);
@@ -25,11 +16,4 @@ export async function appRoutes(app: FastifyInstance) {
     app.post("/bolsa", BolsaController.createBolsa);
     app.put("/bolsa/:id", BolsaController.updateBolsa);
     app.delete("/bolsa/:id", BolsaController.deleteBolsa);
-
-    // Rotas para PecaCadastrada
-    app.get("/peca", PecaCadastradaController.getPecas);
-    app.get("/peca/:id", PecaCadastradaController.getPeca);
-    app.post("/peca", PecaCadastradaController.createPeca);
-    app.put("/peca/:id", PecaCadastradaController.updatePeca);
-    app.delete("/peca/:id", PecaCadastradaController.deletePeca);
 }
