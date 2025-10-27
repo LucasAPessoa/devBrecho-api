30615645dadabeb6002f80c8d6c81208fe94c04a
LucasAPessoa <lucasapessoa@outlook.com>
2025-10-27 11:00:07 -0300

    refactor: remove FornecedoraController and routes of Fornecedoras from routes file as part of code cleanup

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { FornecedoraController } from "./controllers/FornecedoraController";
+
 import { BolsaController } from "./controllers/BolsaController";
 
 export async function appRoutes(app: FastifyInstance) {
-    // Rotas para Fornecedora
-    app.get("/fornecedora", FornecedoraController.getFornecedoras);
-    app.get("/fornecedora/:id", FornecedoraController.getFornecedora);
-    app.post("/fornecedora", FornecedoraController.createFornecedora);
-    app.put("/fornecedora/:id", FornecedoraController.updateFornecedora);
-    app.delete("/fornecedora/:id", FornecedoraController.deleteFornecedora);
-
     // Rotas para Bolsa
     app.get("/bolsa", BolsaController.getBolsas);
     app.get("/bolsa/:id", BolsaController.getBolsa);
